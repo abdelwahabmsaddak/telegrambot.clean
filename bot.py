e8e7fba53bad71d16ac458d82af0e1c8c174dbe8
abdelwahabmsaddak <abdelwahabmsaddak@gmail.com>
2026-01-14 06:38:55 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
+print("BOT FILE LOADED")
+
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, MessageHandler, filters
 
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ البوت يخدم توّا و يجاوب")
+async def echo(update: Update, context):
+    await update.message.reply_text("📩 وصلني مساجك")
 
-def main():
-    app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling(drop_pending_updates=True)
+app = Application.builder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT, echo))
 
-if __name__ == "__main__":
-    main()
+print("RUNNING POLLING")
+app.run_polling()
